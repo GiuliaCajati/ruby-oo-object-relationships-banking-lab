d204e12a0287623dd25726e1658dd3a0fb2faacd
conrad_buys <conradbuys11@gmail.com>
2020-08-28 15:27:37 -0400

    finished assignment

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,48 @@
 class BankAccount
 
+    attr_accessor :balance, :status
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+    end
+
+    def deposit(amount)
+        self.balance += amount
+    end
+
+    def display_balance
+       "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+        self.status == "open" && self.balance > 0
+    end
+
+    def close_account
+        self.status = "closed"
+    end
+
 end
+
+# BankAccount
+
+# Transfer
+#   initialize
+#     can initialize a Transfer (FAILED - 5)
+#     initializes with a sender (FAILED - 6)
+#     initializes with a receiver (FAILED - 7)
+#     always initializes with a status of 'pending' (FAILED - 8)
+#     initializes with a transfer amount (FAILED - 9)
+#   #valid?
+#     can check that both accounts are valid (FAILED - 10)
+#     calls on the sender and receiver's #valid? methods (FAILED - 11)
+#   #execute_transaction
+#     can execute a successful transaction between two accounts (FAILED - 12)
+#     each transfer can only happen once (FAILED - 13)
+#     rejects a transfer if the sender does not have enough funds (does not have a valid account) (FAILED - 14)
+#   #reverse_transfer
+#     can reverse a transfer between two accounts (FAILED - 15)
+#     it can only reverse executed transfers (FAILED - 16)
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,45 @@
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :status, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+
+  def valid?
+    self.receiver.valid? && self.sender.valid?
+  end
+
+  def execute_transaction()
+    if self.valid? && self.status == "pending" && self.sender.balance > self.amount
+      self.receiver.deposit(amount)
+      self.sender.deposit(-amount)
+      self.status = "complete"
+    else
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if self.status == "complete"
+      #put money back in sender
+      #take away money from receiver
+      self.sender.balance += self.amount
+      self.receiver.balance -= self.amount
+      self.status = "reversed"
+    end
+  end
+
 end
+
+
+# BankAccount
+
+# Transfer
+#   #reverse_transfer
+#     can reverse a transfer between two accounts (FAILED - 15)
+#     it can only reverse executed transfers (FAILED - 16)
